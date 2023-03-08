0b070f19812df8298729fb29e1e64fc8cce00abb
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-03-08 18:17:51 +0530

    [Amol] : Adding for BubbleSort Array

---
+++ DataStructure/DataStructure/BubbleSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class BubbleSort
+    {
+        //public void DisplayBubbleSort()
+        //{
+        //    int[] arr = { 78, 55, 45, 98, 13 };
+        //    int temp;
+        //    for (int j = 0; j <= arr.Length; j++)
+        //    {
+        //        for (int i = 0; i <= arr.Length; i++)
+        //        {
+        //            if (arr[i] == arr[i + 1])
+        //            {
+        //                temp = arr[i + 1];
+        //                arr[i + 1] = arr[i];
+        //                arr[i] = temp;
+        //            }
+        //        }
+        //    }
+        //    Console.WriteLine("sorted : ");
+        //    foreach (int p in arr)
+        //    {
+        //        Console.WriteLine(p);
+        //    }
+        //}
+        public void DisplayBubbleSort()
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // Swap the elements
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            //print array
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+}
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BubbleSort sort = new BubbleSort();
+            sort.DisplayBubbleSort();
+            Console.ReadLine();
+        }
+    }
+}
